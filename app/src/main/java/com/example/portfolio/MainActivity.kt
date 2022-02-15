552e0eabbef948ac32fd86aa10e17134883d5fe5
master <ray.yoon87@gmail.com>
2022-02-15 04:06:52 -0600

    Viewmodel SavedState is applied.

--- app/src/main/java/com/example/portfolio/MainActivity.kt
+++ app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.portfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResume() {
+        setupNavigation()
+
+        super.onResume()
+    }
+
+    private fun setupNavigation(){
+        //attempting to retrieve the NavController in onCreate() of an Activity via Navigation.
+        // findNavController(Activity, @IdRes int) will fail.
+        // You should retrieve the NavController directly from the NavHostFragment instead.
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //inflate the men; this adds items to the action bar if it is present
+        //menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/portfolio/view/HomeFragment.kt
@@ -0,0 +1,61 @@
+package com.example.portfolio.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import com.example.portfolio.R
+import com.example.portfolio.viewmodel.HomeFragment_VM
+import com.example.portfolio.viewmodel.HomeFragment_VM.Companion.SAVED_EDIT_TEXT
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+
+class HomeFragment : Fragment(),View.OnClickListener {
+    private lateinit var navController:NavController
+    private val vm:HomeFragment_VM by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupNavController()
+    }
+
+
+    private fun setupNavController(){
+        navController = findNavController(this)
+    }
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val action = HomeFragmentDirections.actionHomeFragmentToMyAppListFragment()
+
+        view.toNext.setOnClickListener(this)
+
+        return view
+    }
+
+    override fun onClick(view: View?) {
+
+        when(view){
+            hometitle_edittext -> {
+                val tempText = hometitle_edittext.text
+                vm.saveState(HomeFragment_VM.SAVED_EDIT_TEXT, tempText)
+            }
+            toNext -> {
+                val action = HomeFragmentDirections.actionHomeFragmentToMyAppListFragment()
+                navController.navigate(action)
+            }
+        }
+    }
+}
